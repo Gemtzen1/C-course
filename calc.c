dc5b877c751b6f86e04f145c8294ef7046f19c11
Gemtzen1 <153219933+Gemtzen1@users.noreply.github.com>
2024-05-06 13:44:43 +0200

    Add files via upload

---
+++ calc.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int num1,num2,total;
+
+main()
+{
+
+
+scanf("%d %d\n",&num1,&num2);
+
+
+
+total=num1*num1;
+
+
+
+printf("Multiplication is %d\n",total);
+
+
+total= num1/num2;
+
+
+printf("Division is %d\n",total);
+
+
+total=num1-num2;
+
+printf("Substraction is %d\n",total);
+
+
+total=num1+num2;
+
+printf("Addition is %d\n",total);
+
+
+return 0;
+
+}

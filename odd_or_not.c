d0ed4ad5aed0d172724746014e6f416c708b0732
Gemtzen1 <Filemon7@proton.me>
2024-10-25 08:49:23 +0200

    Add files via upload

---
+++ odd_or_not.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+
+int number;
+
+scanf("%d",&number);
+
+if(number%2==0)
+{
+
+
+printf("not odd %d\n",number);
+
+}
+else
+{
+
+printf("number is odd %d\n",number);
+
+
+}
+
+
+
+return 0;
+}

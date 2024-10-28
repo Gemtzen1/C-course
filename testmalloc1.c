274f62bbf45a733f49d050fb49135de4216c54fe
Gemtzen1 <Filemon7@proton.me>
2024-10-28 17:15:43 +0100

    Add files via upload

---
+++ testmalloc1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void *malloc(sizeof,char*7)=char array1[7]={'a','b','c','d','e','f'};
+
+int main()
+{
+
+
+
+while(array1>6)
+             {
+
+printf("array %d",array1);
+
+  }
+
+
+
+
+
+
+
+
+return 0;
+}
